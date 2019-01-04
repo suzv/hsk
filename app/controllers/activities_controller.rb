d6b1664b33c4fd91e34021d9d1e57db873c80a97
susana <susana.zv@gmail.com>
2019-01-04 08:53:40 -0300

    added class to words in show view

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,11 @@
 class ActivitiesController < ApplicationController
   def show
+    @word = Activity.new.random_word
+    @meaning = [@word.meaning, Activity.new.random_meaning, Activity.new.random_meaning].shuffle
+    @pinyin = [@word.pinyin, Activity.new.random_pinyin, Activity.new.random_pinyin].shuffle
+  end
+
+  def check_result
     
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,22 @@ class Activity < ApplicationRecord
   belongs_to :users
 
   def random_word
-    
+    random_offset = rand(Word.first.id..Word.last.id)
+    random_word = Word.find(random_offset)
+  end
 
+  def random_meaning
+    random_offset = rand(Meaning.first.id..Meaning.last.id)
+    random_meaning = Meaning.find(random_offset)
+  end
+
+\
+  def random_pinyin
+    random_offset = rand(Pinyin.first.id..Pinyin.last.id)
+    random_pinyin = Pinyin.find(random_offset)
+  end
+  
+  def shuffle
+    sort_by { rand }
   end
 end
